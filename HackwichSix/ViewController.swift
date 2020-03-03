7cfe21bdf843dc2a81fb9ae74e9728e040e71e54
Ashley Aurellano <ashley.aurellano@hotmail.com>
2020-03-03 10:38:57 -1000

    Completed Parts 1-10 and Problem Set #1

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+/*UITableViewDataSource relates to the actual data being shown in your table
+ UITableViewDelegate deals more with the appearance and UI of the table.*/
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    var myFriendsArray = ["Rachel", "Jaymie", "Kristi", "Tracy"]
+    
+    //Problem Set #1
+    var myBucketList = ["New York", "Barcelona", "Japan"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+     //we create a cell and fill that cell with data from the myFriendsArray
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+     {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        let text = myBucketList[indexPath.row]
+        cell?.textLabel?.text = text
+        return cell!
+     }
+    
+    //returns the amount of rows that corresponds to the number of items in your array.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return myBucketList.count
+     }
 }
 
